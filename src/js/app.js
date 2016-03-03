cc1bfcaa768b8c9c83749bc0fdd145e881f16007
Tiago Celestino <tcelestino@gmail.com>
2016-03-03 07:07:05 -0300

    binding form element on Moip Forms component

--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,7 @@
   var form = document.querySelector('.form');
   var celMask = ['+99(99)9999-99999', '+99(99)99999-9999'];
   var cel = document.getElementById('cel');
-  
+
   VMasker(document.getElementById('cpf')).maskPattern('999.999.999-99');
   VMasker(cel).maskPattern(celMask[0]);
 
@@ -15,8 +15,7 @@
     false);
 
   form.addEventListener('submit', function (evt) {
-    var fields = this.elements;
-    Moip.Forms.init(fields);
+    Moip.Forms.init(this);
     evt.preventDefault();
   }, true);
 }());
